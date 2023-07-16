e9491d03a7d2fc1248a63fbc56c484d8da2de8c3
Prem Sagar <50700219+premsagar788@users.noreply.github.com>
2023-07-16 14:31:14 +0500

    added slider
    removed hero img, added slider and fixed styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,32 +7,38 @@
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
 
-<div class="container col-xxl-8 px-4 py-5">
-    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-      <div class="col-10 col-sm-8 col-lg-6">
-        <img src="bootstrap-themes.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
-      </div>
-      <div class="col-lg-6">
-        <h1 class="display-5 fw-bold lh-1 mb-3">Responsive left-aligned hero with image</h1>
-        <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-          <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
-        </div>
-      </div>
+<div id="carouselExampleControls" class="carousel slide pb-5" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://placehold.co/600x200" class="d-block w-100" src="..." alt="First slide">
     </div>
-</div>
-  
+    <div class="carousel-item">
+      <img src="https://placehold.co/600x200" class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img src="https://placehold.co/600x200" class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>  
 
 <div class="container cta-100 ">
     <div class="container">
       <div class="row blog">
         <div class="col-md-12">
           <div id="blogCarousel" class="carousel slide container-blog" data-ride="carousel">
+            <h1 align="center" class="pb-2">Latest Posts</h1>
             <ol class="carousel-indicators">
               <li data-target="#blogCarousel" data-slide-to="0" class="active"></li>
               <li data-target="#blogCarousel" data-slide-to="1"></li>
-          </ol>
+            </ol>
           <!-- Carousel items -->
           <div class="carousel-inner">
               <div class="carousel-item active">
@@ -41,7 +47,7 @@
                     <div class="item-box-blog">
                       <div class="item-box-blog-image">
                         <!--Date-->
-                        <div class="item-box-blog-date bg-blue-ui white"> <span class="mon">Augu 01</span> </div>
+                        <div class="item-box-blog-date bg-blue-ui white"> <span class="mon">Aug 01</span> </div>
                         <!--Image-->
                         <figure> <img alt="" src="https://cdn.pixabay.com/photo/2017/02/08/14/25/computer-2048983_960_720.jpg"> </figure>
                     </div>
@@ -69,7 +75,7 @@
         <div class="item-box-blog">
           <div class="item-box-blog-image">
             <!--Date-->
-            <div class="item-box-blog-date bg-blue-ui white"> <span class="mon">Augu 01</span> </div>
+            <div class="item-box-blog-date bg-blue-ui white"> <span class="mon">Aug 01</span> </div>
             <!--Image-->
             <figure> <img alt="" src="https://cdn.pixabay.com/photo/2017/02/08/14/25/computer-2048983_960_720.jpg"> </figure>
         </div>
@@ -97,7 +103,7 @@
     <div class="item-box-blog">
       <div class="item-box-blog-image">
         <!--Date-->
-        <div class="item-box-blog-date bg-blue-ui white"> <span class="mon">Augu 01</span> </div>
+        <div class="item-box-blog-date bg-blue-ui white"> <span class="mon">Aug 01</span> </div>
         <!--Image-->
         <figure> <img alt="" src="https://cdn.pixabay.com/photo/2017/02/08/14/25/computer-2048983_960_720.jpg"> </figure>
     </div>
