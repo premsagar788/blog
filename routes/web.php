ffd1bb9964f92315176729d95227aba6ec379cb2
Prem Sagar <50700219+premsagar788@users.noreply.github.com>
2023-07-17 19:50:30 +0500

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/admin/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('home');
 
 Route::get('/blog', [App\Http\Controllers\FrontendController::class, 'blog']);
