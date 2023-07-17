b083824c1b3f11d93984509b3e21541e2ef248d0
Prem Sagar <50700219+premsagar788@users.noreply.github.com>
2023-07-17 19:49:49 +0500

    small fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,7 +11,7 @@ class DashboardController extends Controller
     public function index()
     {
         $posts = Post::all();
-        return view('posts.index', compact('posts'));
+        return view('admin.dashboard', compact('posts'));
     }
 
     public function create()
